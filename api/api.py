542d62b3fe6512dcb1b8a26ac5f5090e19cbec81
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2022-10-31 02:27:48 -0500

    Actually works now

--- api/api.py
+++ api/api.py
@@ -1,12 +1,4 @@
-import json
-import operations
+import main
 
 def application(env, start_response):
-    #print(env["REQUEST_METHOD"])
-    if 'HTTP_DATA' in env:
-        print(json.loads(env["HTTP_DATA"])['operation'])
-        start_response('200 OK',[('Content-Type','text/plain;charset=utf-8')])
-        return [b"{'rz':'true'}"]
-    else:
-        start_response('400 NO DATA', [('Content-Type', 'text/plain;charset=utf-8')])
-        return [b"{'rz':'false'}"]
+    return main.run(env,start_response)
--- api/operations.py
+++ api/operations.py
@@ -1,3 +1,7 @@
-# as of now, nothing
+import cython
+from auth import Auth
 
-ourList = []
+# Keep human-readable names in 1st, actual in 2nd
+opNames = ('Auth','GetSomething')
+# Data stored in (GET,POST,PUT,DELETE), and each is a function
+opFuncs = (Auth,Auth) # The second Auth is just a placeholder
